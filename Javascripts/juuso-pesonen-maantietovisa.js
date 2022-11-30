5cbc8c576f7dcc3d74df53bc42e2db9b9fc111ca
JuusoPesonen <c2peju00@students.oamk.fi>
2022-11-30 12:54:15 +0200

    Lisätty visan kysymykset js tiedostoon

--- Javascripts/juuso-pesonen-maantietovisa.js
+++ Javascripts/juuso-pesonen-maantietovisa.js
@@ -1,1 +1,41 @@
 //Tekijä: Juuso Pesonen
+
+//Questions list of arrays
+
+const quiz = [
+    {
+        question: " Missä päin Suomea Olavinlinna sijaitsee?",
+        options: ["Turussa", "Hämeenlinnassa", "Savonlinnassa", "Tampereella"],
+        answer: 2,
+        imageUrl: "/Images/Olavinlinna selitekuva.png",
+
+    },
+    {
+        question: "Missä maanosassa Japani on?",
+        options: ["Euroopassa", "Etelä-Amerikassa", "Afrikassa", "Aasiassa"],
+        answer: 3,
+        imageUrl: "/Images/Aasia selitekuva.png",
+    
+    },
+    {   question: "Missä maassa Venetsia on?",
+        options: ["Ranska", "Italia", "Irlanti", "Tanska"],
+        answer: 1,
+        imageUrl: "/Images/Italia selitekuva.png",
+
+    },
+    {
+        question: "Mihin maakuntaan Kuopio kuuluu?",
+        options: ["Etelä-Savo", "Pohjois-Savo", "Keski-Suomi", "Kanta-Häme"],
+        answer: 1,
+        imageUrl: "/Images/Pohjois-Savo selitekuva.png",
+
+    },
+    {
+        question: "Mikä on Yhdysvaltojen pääkaupunki?",
+        options: ["New York", "Los Angeles", "Washington D.C", "Philadelphia"],
+        answer: 2,
+        imageUrl: "/Images/Washington selitekuva.png",
+
+    }
+
+]
