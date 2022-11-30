c9cd2fdaf0cd083b0f4f45cdc5f16d1f3548b3c1
JuusoPesonen <c2peju00@students.oamk.fi>
2022-11-30 16:15:08 +0200

    Lisätty js koodiin kysymykset ja toiminnallisuus

--- Javascripts/juuso-pesonen-maantietovisa.js
+++ Javascripts/juuso-pesonen-maantietovisa.js
@@ -39,3 +39,132 @@ const quiz = [
     }
 
 ]
+
+//Get questions and answers parameters
+
+const questionNum = document.querySelector(".question-num");
+const questionText = document.querySelector(".question-text");
+const optionContent = document.querySelector(".options-content");
+const answerBox = document.querySelector(".question-image");
+
+// Create global parameters
+
+let questionCount = 0;
+let currentQuestion;
+let answerText;
+let chosenQuestions = [];
+let chosenOptions = [];
+
+//Setting question parameters into chosenQuestions array
+function setChosenQuestions() {
+    const totalQuestions = quiz.length;
+    for(let i = 0; i < totalQuestions; i++) {
+        chosenQuestions.push(quiz[i]);
+    }
+}
+
+//Set question number and questions and options from array
+function getNewQuestion() {
+    //Question number
+    questionNum.innerHTML = "Kysymys " + (questionCount + 1) + " / " + quiz.length;
+
+    //Question text
+    // Get random question
+    const questionIndex = chosenQuestions[Math.floor(Math.random()* chosenQuestions.length)];
+    currentQuestion = questionIndex;
+
+    //Showing question
+    questionText.innerHTML = currentQuestion.question;
+    //console.log(questionIndex);
+
+    //position of "questionIndex" from the chosenQuestions array
+    const indexNum1 = chosenQuestions.indexOf(questionIndex);
+
+    //remove the "questionIndex" from the chosenQuestions array, so no repeat going to happen
+    chosenQuestions.splice(indexNum1, 1);
+
+    // set options for the question
+    //get length of options
+    const optionLength = currentQuestion.options.length;
+
+    //push options into chosenOptions array
+    for( let i = 0; i < optionLength; i++) {
+        chosenOptions.push(i)
+    }
+
+    //Empty questions that they don't add more when nextQuestion has been clicked
+    optionContent.innerHTML = '';
+
+    //animation for options
+    let animationDelay = 0.15;
+
+    //create options in html
+    for ( let i = 0; i < optionLength; i++) {
+
+        //random option from question
+        const optionIndex = chosenOptions[Math.floor(Math.random() * chosenOptions.length)];
+
+        // get the position of "optionIndex" from the chosenOptions
+        const indexNum2 = chosenOptions.indexOf(optionIndex);
+        //remove the "optionIndex" from the chosenOptions array, so that no repeat going to happen on options
+        chosenOptions.splice(indexNum2, 1);
+
+        console.log(optionIndex)
+
+        //adding options to questions
+        const option = document.createElement("div");
+        option.innerHTML = currentQuestion.options[optionIndex];
+        option.id = optionIndex;
+        option.style.animationDelay = animationDelay + "s";
+        animationDelay = animationDelay + 0.15;
+        option.className = "option";
+        optionContent.appendChild(option);
+        option.setAttribute("onclick", "getResult(this)");
+    }
+
+    //console.log(chosenOptions)
+    //console.log(currentQuestion.options)
+
+    questionCount++;
+}
+
+// Get the result of current question attempt
+function getResult(optionElement) {
+    const id = parseInt(optionElement.id);
+
+    //get the answer from id of clicked option
+    if ( id === currentQuestion.answer) {
+        // Set the green color to the correct answer
+        optionElement.classList.add("correct");
+    } else {
+        // Set the red color to the wrong answer
+        optionElement.classList.add("wrong");
+    }
+
+    unclickableOptions();
+}
+
+//Parameters when can't choose other options when one option have been chosen
+function unclickableOptions() {
+    const optionLength = optionContent.children.length;
+    for( let i = 0; i < optionLength; i++) {
+        optionContent.children.classList[i].add("already-chosen");
+    }
+}
+
+//Go to next question function on button
+function nextQuestion() {
+    if( questionCount === quiz.length) {
+        //console.log("quiz over")
+    } else {
+        getNewQuestion();
+    }
+}
+
+//Show questions on window
+window.onload = function() {
+    // set all question in chosenQuestions array
+    setChosenQuestions();
+    // then call getNewQuestion function
+    getNewQuestion();
+}
