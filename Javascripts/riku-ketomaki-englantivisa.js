74d36d6a802b49e297f8d5119c5636c4f9b985fc
RikuKetomaki <riku.ketomaki@gmail.com>
2022-12-09 12:12:00 +0200

    Added comments and check if anwser field is right

--- Javascripts/riku-ketomaki-englantivisa.js
+++ Javascripts/riku-ketomaki-englantivisa.js
@@ -1,8 +1,12 @@
+// Define variables
 const all_options = document.querySelectorAll(".option");
 const fields = document.querySelectorAll("#field")
+const anwser_fields = document.querySelectorAll(".anwserField");
+const explonation_field = document.querySelector("#explonation");
 
 let draggedItem = null;
 
+// Options interractions when starting / ending dragging
 for (let i = 0; i < all_options.length; i++) {
     const item = all_options[i];
 
@@ -21,14 +25,13 @@ for (let i = 0; i < all_options.length; i++) {
         }, 0);
     })
 
+    // fields drag event interractions
     for (let j = 0; j < fields.length; j++) {
         const field = fields[j];
 
         field.addEventListener("dragover", function (e) {
             e.preventDefault ();
         })
-
-
 
         field.addEventListener("dragover", function (e) {
             e.preventDefault ();
@@ -42,6 +45,12 @@ for (let i = 0; i < all_options.length; i++) {
         field.addEventListener("drop", function (e) {
             this.append(draggedItem);
             this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+
+            // Check if field that object has been dropped is anwser field
+            if (e.target.className == "anwser-field") {
+                console.log("dropped");
+                
+            }
         });
     }
 }
