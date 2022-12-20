8bb7f68dbb4d22bd675bae8851d1edeef8fc25e0
Tuomas K <c1katu01@students.oamk.fi>
2022-12-20 08:51:57 +0200

    historiavisa

---
+++ Javascripts/tuomas-kärki-historiavisa.js
@@ -0,0 +1,28 @@
+// let quizStart = document.getElementById("startQuiz");
+
+// let firstQuestion = document.getElementById("question1Main");
+// let secondQuestion = document.getElementById("question2Main");
+// let thirdQuestion = document.getElementById("question3Main");
+// let fourthQuestion = document.getElementById("question4Main");
+// let fifthQuestion = document.getElementById("question5Main");
+
+// let checkAnswer1 = document.getElementById("answerCheckButton1");
+// let checkAnswer2 = document.getElementById("answerCheckButton2");
+// let checkAnswer3 = document.getElementById("answerCheckButton3");
+// let checkAnswer4 = document.getElementById("answerCheckButton4");
+// let checkAnswer5 = document.getElementById("answerCheckButton5");
+
+// let nextQuestion1 = document.getElementById("nextQuestionButton1");
+// let nextQuestion2 = document.getElementById("nextQuestionButton2");
+// let nextQuestion3 = document.getElementById("nextQuestionButton3");
+// let nextQuestion4 = document.getElementById("nextQuestionButton4");
+// let endQuiz = document.getElementById("nextQuestionButton5");
+
+// quizStart.addEventListener("click", startQuizMain);
+
+// function startQuizMain() {
+//     quizStart.classList.add("startHide")
+//     firstQuestion.classList.add("question1Show")
+// }
+
+// ...
